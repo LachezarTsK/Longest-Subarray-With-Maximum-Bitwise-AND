987f7713fd0ad17fd1f529ccae234bbb26e174e9
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-09-14 13:44:43 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,25 @@
+
+/**
+ * @param {number[]} input
+ * @return {number}
+ */
+var longestSubarray = function (input) {
+    // Alternative: let maxValue = Math.max(...input);
+    let maxValue = 0;
+    for (let value of input) {
+        maxValue = Math.max(maxValue, value);
+    }
+
+    let maxSizeSubarrayWithMaxBitwiseAND = 0;
+    let currentSizeSubarrayWithMaxBitwiseAND = 0;
+    for (let i = 0; i < input.length; ++i) {
+        if (input[i] === maxValue) {
+            ++currentSizeSubarrayWithMaxBitwiseAND;
+            maxSizeSubarrayWithMaxBitwiseAND = Math.max(maxSizeSubarrayWithMaxBitwiseAND, currentSizeSubarrayWithMaxBitwiseAND);
+        } else {
+            currentSizeSubarrayWithMaxBitwiseAND = 0;
+        }
+    }
+
+    return maxSizeSubarrayWithMaxBitwiseAND;
+};
